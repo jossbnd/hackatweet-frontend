06b30bcc6cd4679ae700700224dffa4852c3f2ef
Hmidux <hameir.hmida@gmail.com>
2022-10-06 17:32:32 +0200

    add modules home

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,36 @@
-import styles from '../styles/Home.module.css';
+import React from "react";
+import styles from "../styles/Home.module.css";
 
-function Home() {
+export default function home() {
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className={styles.container}>
+      <div className={styles.blockLeft}>
+        <img
+          src="/images/logo.png"
+          alt="Logo"
+          className={`${styles.logo} 
+        ${styles.small}`}
+        />
+        <div className={styles.disconnectedButton}>
+          <div className={styles.blockhaut}>
+            <div>
+              <img className={styles.avatar} src="/images/avatar.png"></img>
+            </div>
+            <div className={styles.blocktopright}>
+              <div className={styles.pseudo}>John</div>
+              <div className={styles.username}>@John Cenna</div>
+            </div>
+          </div>
+          <button className={styles.button}>Logout</button>
+
+          <div></div>
+        </div>
+      </div>
+      <div className={styles.blockCenter}>
+        <div className={styles.newmessage}>Zone news message</div>
+        <div className={styles.zonemessage}>Zone message déja posté</div>
+      </div>
+      <div className={styles.blockRight}>block droite</div>
     </div>
   );
 }
-
-export default Home;
