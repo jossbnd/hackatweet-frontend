de4711e53872d46b3ae283ca44deddd4624901be
Josselin Bonnaud <josselin.bonnaud@gmail.com>
2022-10-06 15:51:47 +0200

    add modal

--- components/Welcome.js
+++ components/Welcome.js
@@ -6,18 +6,108 @@ import { Modal } from 'antd';
 function Welcome() {
     const [isModalVisible, setIsModalVisible] = useState(false);
 
-    const showModal = () => {
+    const [signUpIsClicked, setSignUpIsClicked] = useState(false)
+    const [signUpFirstname, setSignUpFirstname] = useState('');
+    const [signUpUsername, setSignUpUsername] = useState('');
+    const [signUpPassword, setSignUpPassword] = useState('');
+
+    const [signInIsClicked, setSignInIsClicked] = useState(false)
+    const [signInUsername, setSignInUsername] = useState('');
+    const [signInPassword, setSignInPassword] = useState('');
+
+    // FONCTIONS
+    const handleSignUp = () => {
         setIsModalVisible(true);
-        console.log(isModalVisible)
+        setSignUpIsClicked(true);
+    };
+
+    const handleSignIn = () => {
+        setIsModalVisible(true);
+        setSignInIsClicked(true);
+    };
+
+    const handleSignUpSubmit = () => {
+        fetch('http://localhost:3000/users/signup', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ firstname: signUpFirstname, username: signUpUsername, password: signUpPassword})
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setIsModalVisible(false);
+            setSignUpIsClicked(false);
+    
+            setSignUpFirstname('');
+            setSignUpUsername('');
+            setSignUpPassword('');
+        })
       };
 
-    const handleOk = () => {
-        setIsModalVisible(false);
+    const handleSignInSubmit = () => {
+        fetch('http://localhost:3000/users/signin', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username: signInUsername, password: signInPassword})
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setIsModalVisible(false);
+            setSignInIsClicked(false);
+    
+            setSignInUsername('');
+            setSignInPassword('');
+        })
       };
     
     const handleCancel = () => {
         setIsModalVisible(false);
+        setSignUpIsClicked(false);
+        setSignInIsClicked(false);
+
+        setSignUpFirstname('');
+        setSignUpUsername('');
+        setSignUpPassword('');
+
+        setSignInUsername('');
+        setSignInPassword('');
     };
+
+
+
+
+    // CONTENU DU MODAL
+    let connectionWindow;
+
+    if (signUpIsClicked) {
+    connectionWindow = <div className={styles.connectionWindow}>
+        <div className={styles.cancelButtonContainer}>
+            <button className={styles.cancelButton} onClick={handleCancel}>X</button>
+        </div>
+        <img className={`${styles.logo} ${styles.small}`} src='/images/logo.png' />
+        <h3 className={styles.modalH3}>Create your Hackatweet account</h3>
+        <input className={styles.logInput} placeholder='Firstname' onChange={(el) => setSignUpFirstname(el.target.value)} value={signUpFirstname}/>
+        <input className={styles.logInput} placeholder='Username' onChange={(el) => setSignUpUsername(el.target.value)} value={signUpUsername}/>
+        <input type='password' className={styles.logInput} placeholder='Password' onChange={(el) => setSignUpPassword(el.target.value)} value={signUpPassword}/>
+        <button className={styles.connectionWindowbutton} onClick={handleSignUpSubmit}>SIGN UP</button>
+    </div>
+    }
+
+    if (signInIsClicked) {
+        connectionWindow = <div className={styles.connectionWindow}>
+        <div className={styles.cancelButtonContainer}>
+            <button className={styles.cancelButton} onClick={handleCancel}>X</button>
+        </div>
+        <img className={`${styles.logo} ${styles.small}`} src='/images/logo.png' />
+        <h3 className={styles.modalH3}>Connect to Hackatweet</h3>
+        <input className={styles.logInput} placeholder='Username' onChange={(el) => setSignInUsername(el.target.value)} value={signInUsername}/>
+        <input type='password' className={styles.logInput} placeholder='Password' onChange={(el) => setSignInPassword(el.target.value)} value={signInPassword}/>
+        <button className={styles.connectionWindowbutton} onClick={handleSignInSubmit}>SIGN IN</button>
+    </div>
+
+    }
+    
 
     return (
         <div className={styles.main}>
@@ -31,17 +121,15 @@ function Welcome() {
                 <h1 className={styles.h1}>See what's happening</h1>
                 <h3 className={styles.h3}>Join Hackatweet today.</h3>
                 <div className={styles.log}>
-                    <button className={`${styles.button} ${styles.logIn}`} onClick={showModal}>SIGN IN</button>
+                    <button className={`${styles.button} ${styles.signUp}`} onClick={handleSignUp}>SIGN UP</button>
                     <p>Already have an account?</p>
-                    <button className={`${styles.button} ${styles.logOut}`}>SIGN UP</button>
+                    <button className={`${styles.button} ${styles.signIn}`} onClick={handleSignIn}>SIGN IN</button>
                 </div>
             </div>
 
-            {isModalVisible && <div className={styles.connection} id='react-modals'>
-                <Modal getContainer="#react-modals" className={styles.modal} title="Basic Modal" open={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
+            {isModalVisible && <div className={styles.modalContainer} id='react-modals'>
+                <Modal getContainer="#react-modals" open={isModalVisible} closable={false} footer={null}>
+                    {connectionWindow}
                 </Modal>
 
             </div>
