fd83012c9795be83b0b5ce995fd94a615856410a
Josselin Bonnaud <josselin.bonnaud@gmail.com>
2022-10-06 10:18:15 +0200

    add redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+ reducer: {},
+});
+
 function App({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <title>Next.js App</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
